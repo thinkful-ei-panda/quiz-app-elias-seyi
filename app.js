f3929c9e241e42ac37aa8d9f9da4aca3dd14201e
Elias Martinez <jemart218@gmail.com>
2020-06-03 13:03:14 -0700

    merge

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
 /**
  * Example store structure
  */
+'use strict';
 const store = {
-  // 5 or more questions are required
-  questions: [
-    {
-      question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
-      ],
-      correctAnswer: 'green'
-    },
-    {
-      question: 'What is the current year?',
-      answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
-    }
-  ],
-  quizStarted: false,
-  questionNumber: 0,
-  score: 0
+// 5 or more questions are required
+questions: [
+  { 
+    question: 'What color is broccoli?',
+    answers: [
+      'red',
+      'orange',
+      'pink',
+      'green'
+    ],
+    correctAnswer: 'green'
+  },
+  {
+    question: 'What is the current year?',
+    answers: [
+      '1970',
+      '2015',
+      '2019',
+      '2005'
+    ],
+    correctAnswer: '2019'
+  },
+  {
+    question: ''
+  }
+],
+quizStarted: false,
+questionNumber: 0,
+score: 0
 };
 
 /**
